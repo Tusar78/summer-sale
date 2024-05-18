0d0297081ee4ec968c1686837a5f77b123ea32c0
Tusar <tusarahmed141@gmail.com>
2024-05-18 17:14:41 +0600

    Price calculation done

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,19 +15,50 @@ toggleBtn.addEventListener("click", function () {
 
 // All neccessary reference for cart
 const productItems = document.querySelectorAll(".product__item");
-const productDprice = document.querySelector('.product__Dprice');
-const totalPrice = document.querySelector('.total__price');
-const cartBox = document.querySelector('.cart__box');
-const purchageBtn = document.querySelector('.purchage-btn');
+const totalProductPrice = document.querySelector(".product__Tprice");
+const productDiscount = document.querySelector(".product__Dprice");
+const totalAmmount = document.querySelector(".total__price");
+const cartBox = document.querySelector(".cart__box");
+const purchageBtn = document.querySelector(".purchage-btn");
+const cuponField = document.querySelector(".coupon__field");
+const cuponBtn = document.querySelector(".coupon__btn");
 
 let itemCount = 0;
+let totalPrice = 0;
+let discountPrice = "0";
+let mainTotal = "0";
 productItems.forEach((item) => {
-  item.addEventListener("click", function (e) {1
+  item.addEventListener("click", function (e) {
+    1;
     itemCount += 1;
     let productName = this.children[1].children[1].innerText;
     let productPrice = +this.children[1].children[2].children[0].innerText;
-    console.log(productName);
-    console.log(productPrice);
-    console.log(itemCount);
+
+    // Total Product Price
+    totalPrice += productPrice;
+    totalProductPrice.innerText = totalPrice;
+
+    // Total Price
+    mainTotal = totalPrice - discountPrice;
+    totalAmmount.innerText = mainTotal;
+  });
 });
+
+cuponField.addEventListener("keyup", function (e) {
+  const DISCOUNT = 20;
+
+  if (e.target.value == "SELL200" && totalPrice >= 200) {
+    discountPrice = (totalPrice * DISCOUNT) / 100;
+    cuponBtn.removeAttribute("disabled");
+    // Total Price
+    mainTotal = totalPrice - discountPrice;
+    totalAmmount.innerText = mainTotal;
+  } else {
+    cuponBtn.setAttribute("disabled", "true");
+    discountPrice = "0"
+    mainTotal = totalPrice - discountPrice;
+    totalAmmount.innerText = mainTotal;
+  }
+
+  productDiscount.innerText = discountPrice;
 });
